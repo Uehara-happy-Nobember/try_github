c0f7cf3be2959ec7e8401e60a9b52efed8c82e6f
MisakiKamiya <misaki-k@occ.co.jp>
2019-05-08 11:59:22 +0900

    add tashizan

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+        public void Tasizan()
+        {
+            int tashi = Number1 + Number2;
+            Console.WriteLine(tasi);
+        }
+	}
+}
